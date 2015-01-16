16a61b75e0d3a5e13e47c1d302546a50cd82d78d
Ubasic <lmjubuntu@gmail.com>
2015-01-16 15:05:46 +0800

    update dataset

--- models/dataset.go
+++ models/dataset.go
@@ -26,3 +26,33 @@ type Dataset struct {
     DataType  int       `xorm:"tinyint(1) default(0)" json:"data_type,omitempty"`
     CreatedAt time.Time `xorm:"created"               json:"created_at,omitempty"`
 }
+
+func (dataset *Dataset) FillObject() (err error) {
+    var tag = &Tag{Id: dataset.TagId}
+    var has bool
+
+    if has, err = engine.Get(tag); err != nil {
+        return
+    } else if has {
+        Dataset.Tag = tag
+    }
+
+    var file = &File{Id: dataset.FileId}
+    if has, err = engine.Get(file); err != nil {
+        return
+    } else if has {
+        Dataset.File = file
+    }
+
+    return
+}
+
+func (dataset *Dataset) SetFile(file *File) {
+    dataset.File = file
+    dataset.FileId = file.Id
+}
+
+func (dataset *Dataset) SetTag(tag *Tag) {
+    dataset.Tag = tag
+    dataset.TagId = tag.Id
+}
