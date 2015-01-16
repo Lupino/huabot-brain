c8d8cb25f2f6a9451b68d2fae474267840f262ea
Ubasic <lmjubuntu@gmail.com>
2015-01-16 11:32:11 +0800

    Add json tag

--- models/file.go
+++ models/file.go
@@ -5,22 +5,24 @@ import (
 )
 
 type File struct {
-    Id        int       `xorm:"pk autoincr"`
-    Key       string    `xorm:"varchar(128) notnull unique"`
-    CreatedAt time.Time `xorm:"created"`
+    Id        int       `xorm:"pk autoincr"                 json:"file_id,omitempty"`
+    Key       string    `xorm:"varchar(128) notnull unique" json:"key,omitempty"`
+    CreatedAt time.Time `xorm:"created"                     json:"created_at,omitempty"`
 }
 
 type Tag struct {
-    Id        int       `xorm:"pk autoincr"`
-    Name      string    `xorm:"varchar(128) notnull unique"`
-    CreatedAt time.Time `xorm:"created"`
+    Id        int       `xorm:"pk autoincr"                 json:"tag_id,omitempty"`
+    Name      string    `xorm:"varchar(128) notnull unique" json:"name,omitempty"`
+    CreatedAt time.Time `xorm:"created"                     json:"created_at,omitempty"`
 }
 
 
 type Dataset struct {
-    Id        int       `xorm:"pk autoincr"`
-    TagId     int       `xorm:"unique(tag_file)"`
-    FileId    int       `xorm:"unique(tag_file)"`
-    Type      int       `xorm:"tinyint(1) default(0)"`
-    CreatedAt time.Time `xorm:"created"`
+    Id        int       `xorm:"pk autoincr"           json:"dataset_id,omitempty"`
+    TagId     int       `xorm:"unique(tag_file)"      json:"tag_id,omitempty"`
+    Tag       *Tag      `xorm:"-"                     json:"tag,omitempty"`
+    FileId    int       `xorm:"unique(tag_file)"      json:"file_id,omitempty"`
+    File      *File     `xorm:"-"                     json:"file,omitempty"`
+    DataType  int       `xorm:"tinyint(1) default(0)" json:"data_type,omitempty"`
+    CreatedAt time.Time `xorm:"created"               json:"created_at,omitempty"`
 }
