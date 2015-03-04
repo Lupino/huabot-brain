38968ec72ad6b6894ecc3a05cb9abc6c27807100
Ubasic <lmjubuntu@gmail.com>
2015-03-04 17:10:04 +0800

    Fixed. the wrong Kill and lock the process

--- tools/train_worker/caffe/caffe.go
+++ tools/train_worker/caffe/caffe.go
@@ -2,39 +2,48 @@ package caffe
 
 import (
     "os"
+    "sync"
     "os/exec"
 )
+
+var Locker = new(sync.Mutex)
 
 var CAFFE = "caffe"
 var COMPUTER_IMAGE_MEAN = "compute_image_mean"
 var CONVERT_IMAGESET = "convert_imageset"
 
-var currentProcess *os.Process
+var currentCmd *exec.Cmd
 
 func Run(args... string) error {
-    cmd := exec.Command(CAFFE, args...)
-    cmd.Stdout = os.Stdout
-    cmd.Stderr = os.Stderr
-    currentProcess = cmd.Process
-    return cmd.Run()
+    Locker.Lock()
+    defer Locker.Unlock()
+    currentCmd = exec.Command(CAFFE, args...)
+    currentCmd.Stdout = os.Stdout
+    currentCmd.Stderr = os.Stderr
+    return currentCmd.Run()
 }
 
 func ComputeImageMean(sourceFile, binaryFile string) error {
-    cmd := exec.Command(COMPUTER_IMAGE_MEAN, sourceFile, binaryFile)
-    cmd.Stdout = os.Stdout
-    cmd.Stderr = os.Stderr
-    currentProcess = cmd.Process
-    return cmd.Run()
+    Locker.Lock()
+    defer Locker.Unlock()
+    currentCmd = exec.Command(COMPUTER_IMAGE_MEAN, sourceFile, binaryFile)
+    currentCmd.Stdout = os.Stdout
+    currentCmd.Stderr = os.Stderr
+    return currentCmd.Run()
 }
 
 func ConvertImageset(args... string) error {
-    cmd := exec.Command(CONVERT_IMAGESET, args...)
-    cmd.Stdout = os.Stdout
-    cmd.Stderr = os.Stderr
-    currentProcess = cmd.Process
-    return cmd.Run()
+    Locker.Lock()
+    defer Locker.Unlock()
+    currentCmd = exec.Command(CONVERT_IMAGESET, args...)
+    currentCmd.Stdout = os.Stdout
+    currentCmd.Stderr = os.Stderr
+    return currentCmd.Run()
 }
 
-func Kill() (error) {
-    return currentProcess.Kill()
+func Kill() (err error) {
+    if currentCmd != nil && currentCmd.Process != nil {
+        return currentCmd.Process.Kill()
+    }
+    return
 }
