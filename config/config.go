cb47a0fd2033d2a9731d2f89eb5fc1447a66d141
Ubasic <lmjubuntu@gmail.com>
2015-04-12 14:40:11 +0800

    configable resourses

--- config/config.go
+++ config/config.go
@@ -1,8 +1,28 @@
 package config
 
-const (
-    RES = "resourses"
+var (
+    RES string
     UPLOADPATH = "public/upload/"
+    TRAIN_FILE string
+    VAL_FILE string
+    TRAIN_LMDB string
+    VAL_LMDB string
+    MEAN_FILE string
+    SOLVER_FILE string
+    LOG_DIR string
+    PLOT_ROOT string
+    PREDICT_HOST string
+    PREDICT string
+)
+
+var FILE_EXTS = map[string]string{
+    "image/png": ".png",
+    "image/jpeg": ".jpg",
+    "image/gif": ".gif",
+}
+
+func SetResource(source string) {
+    RES = source
     TRAIN_FILE = RES + "/train.txt"
     VAL_FILE = RES + "/val.txt"
     TRAIN_LMDB = RES + "/train_lmdb"
@@ -11,12 +31,9 @@ const (
     SOLVER_FILE = RES + "/solver.prototxt"
     LOG_DIR = RES + "/logs"
     PLOT_ROOT = RES + "/plot"
-    PREDICT_HOST = "http://127.0.0.1:3001"
     PREDICT = RES + "/predict/main.py"
-)
+}
 
-var FILE_EXTS = map[string]string{
-    "image/png": ".png",
-    "image/jpeg": ".jpg",
-    "image/gif": ".gif",
+func SetPredictRoot(root string) {
+    PREDICT_HOST = root
 }
--- main.go
+++ main.go
@@ -4,16 +4,23 @@ import (
     "github.com/go-martini/martini"
     "github.com/martini-contrib/render"
     "github.com/Lupino/huabot-brain/backend"
+    "github.com/Lupino/huabot-brain/config"
     "flag"
     "io/ioutil"
     "net/http"
 )
+
+var resourceName = flag.String("resources", "resourses", "Resources path.")
+var predictRoot = flag.String("predict", "http://127.0.0.1:3001", "Predict host.")
 
 func init() {
     flag.Parse()
 }
 
 func main() {
+    config.SetResource(*resourceName)
+    config.SetPredictRoot(*predictRoot)
+
     mart := martini.Classic()
     mart.Use(render.Renderer(render.Options{
         Charset: "UTF-8",
